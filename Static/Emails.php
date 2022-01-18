c004187b2249389f89c617c092b09c1b9ac2838f
Pythony <contact@pythony.dev>
2022-01-18 16:38:21 +0100

    Fix Emails Tags

--- Static/Emails.php
+++ Static/Emails.php
@@ -5,27 +5,51 @@
     final class Emails {
 
         public static function send($email, $subject, $message, $copy = false) {
-            if(!in_array(\Static\Models\Database::getEnvironment(), array("production"))) return true;
-            else return mail(htmlspecialchars($email), \Static\Languages\Translate::getText("project-name") . " - " . htmlspecialchars($subject), htmlspecialchars($message) . \Static\Languages\Translate::getText("emails-footer"), \Static\Languages\Translate::getText("emails-" . ($copy ? "copy" : "header")));
+            $email = htmlspecialchars($email);
+            $subject = \Static\Languages\Translate::getText("project-name") . " - " . htmlspecialchars($subject);
+            $message = htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-start")) . $message . htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-end"));
+            $headers = htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-" . ($copy ? "copy" : "header")));
+
+            if(\Static\Models\Database::getEnvironment() == "production") return mail($email, $subject, $message, $headers);
+            else if(\Static\Models\Database::getEnvironment() == "development") {
+                print_r(array(
+                    "email" => $email,
+                    "subject" => $subject,
+                    "message" => $message,
+                    "headers" => $headers,
+                ));
+
+                return true;
+            } else return false;
         }
 
         public static function contact($email, $message) {
             $title = \Static\Languages\Translate::getText("emails-contact-title");
-            $content = \Static\Languages\Translate::getText("emails-contact-content-1") . $message . \Static\Languages\Translate::getText("emails-contact-content-2");
+            $content = htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-contact-content-1"));
+            $content .= htmlspecialchars($message);
+            $content .= htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-contact-content-2"));
 
-            return self::send($email, $title, $content);
+            return self::send($email, $title, $content, true);
         }
 
         public static function signUp($email, $password) {
             $title = \Static\Languages\Translate::getText("emails-signUp-title");
-            $content = \Static\Languages\Translate::getText("emails-signUp-content-1") . $email . \Static\Languages\Translate::getText("emails-signUp-content-2") . $password . \Static\Languages\Translate::getText("emails-signUp-content-3");
+            $content = htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-signUp-content-1"));
+            $content .= htmlspecialchars($email);
+            $content .= htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-signUp-content-2"));
+            $content .= htmlspecialchars($password);
+            $content .= htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-signUp-content-3"));
 
             return self::send($email, $title, $content);
         }
 
         public static function reset($email, $password) {
             $title = \Static\Languages\Translate::getText("emails-reset-title");
-            $content = \Static\Languages\Translate::getText("emails-reset-content-1") . $email . \Static\Languages\Translate::getText("emails-reset-content-2") . $password . \Static\Languages\Translate::getText("emails-reset-content-3");
+            $content = htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-reset-content-1"));
+            $content .= htmlspecialchars($email);
+            $content .= htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-reset-content-2"));
+            $content .= htmlspecialchars($password);
+            $content .= htmlspecialchars_decode(\Static\Languages\Translate::getText("emails-reset-content-3"));
 
             return self::send($email, $title, $content);
         }
